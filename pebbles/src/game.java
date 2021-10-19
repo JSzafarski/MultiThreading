4b9dee62f67acddf69dd15cf4c70c6e7a6e8a692
grahamgbf <gbf201@exeter.ac.uk>
2021-10-19 14:47:26 +0100

    error fix
    moved the bag enums outside of the public void start_game() method as this was causing an error due to enums not being allowed to be public when defined inside a method which caused the first enum to be declared to be unable to reference the second enum to be declared as due to being in a method they would be created chronologically as such i moved them and fixed the errors bringing it down from 20 to 4 the 4 remaining errors are syntax related due to work in progress methods we have started.

--- pebbles/src/game.java
+++ pebbles/src/game.java
@@ -5,7 +5,61 @@ import java.util.Random;
 public class game {
     int numPlayers;
 
-    public void start_game(){
+    public enum blackBags {
+        bagX(whiteBags.bagA),
+        bagY(whiteBags.bagB),
+        bagZ(whiteBags.bagC);
+        ArrayList<Integer> pebbles = new ArrayList<Integer>();
+        Random rand = new Random();
+        whiteBags bagPair;
+        //constructors
+        blackBags(whiteBags bagPair) {this.bagPair = bagPair;}
+        blackBags() {}
+        //get and set methods for the list of pebbles in the bags
+        public ArrayList<Integer> getPebbles() {return pebbles;}
+        public void setPebbles(ArrayList<Integer> pebbles) {this.pebbles = pebbles;}
+        //get and set methods for the bag pairs of each bag
+        public whiteBags getBagPair() {return bagPair;}
+        public void setBagPair(whiteBags bagPair) {this.bagPair = bagPair;}
+        //draw pebble from a chosen bag method
+        public int drawPebble() {
+            if (this.getPebbles().size() == 0) {
+                //return -1 if empty list
+                return -1;
+            } else {
+                //else generate a random index for the list used to randomly select a pebble from the list
+                int index = rand.nextInt(this.getPebbles().size());
+                int pebbleWeight = this.getPebbles().get(index);
+                //remove the pebble from the list
+                this.getPebbles().remove(index);
+                //return the weight of the selected pebble
+                return pebbleWeight;
+            }
+        }
+
+    }
+
+    public enum whiteBags {
+        bagA(blackBags.bagX),
+        bagB(blackBags.bagY),
+        bagC(blackBags.bagZ);
+        ArrayList<Integer> pebbles = new ArrayList<Integer>();
+        Random rand = new Random();
+        blackBags bagPair;
+        //constructors
+        whiteBags(blackBags bagPair) {this.bagPair = bagPair;}
+        whiteBags() {}
+        //get and set methods for the list of pebbles in the bags
+        public ArrayList<Integer> getPebbles() {return pebbles;}
+        public void setPebbles(ArrayList<Integer> pebbles) {this.pebbles = pebbles;}
+        //get and set methods for the bag pairs of each bag
+        public blackBags getBagPair() {return bagPair;}
+        public void setBagPair(blackBags bagPair) {this.bagPair = bagPair;}
+        //discard pebble into chosen bag method
+        //public discardPebble(whiteBags thisBag)
+    }
+
+    public void start_game() {
         System.out.println("you will be asked to enter the number of players and then for the location");
         System.out.println("of three files in turn containing comma separated inger values for the pebble weights.");
         System.out.println("the integer must be strictly positive.");
@@ -25,18 +79,18 @@ public class game {
                         file_input = System.console().readLine();//verify data type
                         if (Objects.equals(file_input, "X")) {
                             break;//exit the loop
-                        }else { //check if the input is valid using try catch block
+                        } else { //check if the input is valid using try catch block
                             try () {//reading a file
 
                             } catch () {
                                 System.out.println("invalid input ,we will ask you to repeat your input.");
-                                //if the file read is invalid then repeat the fir loop
+                                //if the file read is invalid then repeat the for loop
                             }
                         }
                         // validate
                     }
                     //calculate the necessary amounts of pebbles for the players
-                    if (!Objects.equals(file_input, "X")){//if X was pressed then the program should repeat everything as the user requested to change inoputs
+                    if (!Objects.equals(file_input, "X")) {//if X was pressed then the program should repeat everything as the user requested to change inputs
                         break;
                     }
                 } else {
@@ -45,67 +99,13 @@ public class game {
             } catch (NumberFormatException e) {
                 System.out.println("invalid input ,enter again!");//shows error
             }
-    }
-
-    public enum blackBags{
-        bagX(whiteBags.bagA),
-        bagY(whiteBags.bagB),
-        bagZ(whiteBags.bagC);
-        ArrayList<Integer> pebbles = new ArrayList<Integer>();
-        Random rand = new Random();
-        whiteBags bagPair;
-        //constructors
-        blackBags(whiteBags bagPair) {this.bagPair = bagPair;}
-        blackBags(){}
-        //get and set methods for the list of pebbles in the bags
-        public ArrayList<Integer> getPebbles() {return pebbles;}
-        public void setPebbles(ArrayList<Integer> pebbles) {this.pebbles = pebbles;}
-        //get and set methods for the bag pairs of each bag
-        public whiteBags getBagPair() {return bagPair;}
-        public void setBagPair(whiteBags bagPair) {this.bagPair = bagPair;}
-
-        //draw pebble from a chosen bag method
-        public int drawPebble() {
-            if (this.getPebbles().size() == 0) {
-                //return -1 if empty list
-                return -1;
-            } else {
-                //else generate a random index for the list used to randomly select a pebble from the list
-                int index = rand.nextInt(this.getPebbles().size());
-                int pebbleWeight = this.getPebbles().get(index);
-                //remove the pebble from the list
-                this.getPebbles().remove(index);
-                //return the weight of the selected pebble
-                return pebbleWeight;
-            }
         }
 
+
+        public void read_csv () {
+        }
+
+
     }
-    public enum whiteBags{
-        bagA(blackBags.bagX),
-        bagB(blackBags.bagY),
-        bagC(blackBags.bagZ);
-        ArrayList<Integer> pebbles = new ArrayList<Integer>();
-        Random rand = new Random();
-        blackBags bagPair;
-        //constructors
-        whiteBags(blackBags bagPair){this.bagPair = bagPair;}
-        whiteBags(){}
-        //get and set methods for the list of pebbles in the bags
-        public ArrayList<Integer> getPebbles() {return pebbles;}
-        public void setPebbles(ArrayList<Integer> pebbles) {this.pebbles = pebbles;}
-        //get and set methods for the bag pairs of each bag
-        public blackBags getBagPair() {return bagPair;}
-        public void setBagPair(blackBags bagPair) {this.bagPair = bagPair;}
-
-        //discard pebble into chosen bag method
-        //public discardPebble(whiteBags thisBag)
-    }
-
-    public void read_csv(){}
-
-
-
-
 
 }
