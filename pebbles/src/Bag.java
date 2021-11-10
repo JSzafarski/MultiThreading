b6844784271ef677754030b097a7e054b6c7d25e
JSzafarski <92590150+JSzafarski@users.noreply.github.com>
2021-11-10 15:12:44 +0000

    all commenting done from me

--- pebbles/src/Bag.java
+++ pebbles/src/Bag.java
@@ -3,18 +3,25 @@ import java.util.Random;
 
 public class Bag {
     /**
-     *
+     * This class allows us to create multiple bag instances for each bag colour and letter
+     * @since v1.0
      */
 
     Random rand = new Random();
     private ArrayList<Integer> pebbles = new ArrayList<Integer>();
     private Bag bagPair;
 
-    public ArrayList<Integer> getPebbles() {return this.pebbles;}
+    public ArrayList<Integer> getPebbles() {
+        /**
+         * This class allows us to create multiple bag instances for each bag colour and letter
+         * @since v1.0
+         */
+        return this.pebbles;}
 
     public void setPebbles(ArrayList<Integer> pebbles) {
         /**
-         *
+         * @param pebbles ,is an integer array that represents the pebbles that are offloaded into the bag integer array
+         * @since v1.0
          */
         int pebblePos = 0;
         while(pebblePos <= pebbles.size()-1){//populates the bag with pebbles
@@ -24,13 +31,27 @@ public class Bag {
     }
 
     //get and set methods for the bag pairs of each bag
-    public Bag getBagPair() {return bagPair;}
-    public void setBagPair(Bag bagPair) {this.bagPair = bagPair;}
+    public Bag getBagPair() {
+        /**
+         * @param pebbles ,is an integer array that represents the pebbles that are offloaded into the bag integer array
+         * @since v1.0
+         */
+        return bagPair;
+    }
+    public void setBagPair(Bag bagPair) {
+        /**
+         * @param pebbles ,is an integer array that represents the pebbles that are offloaded into the bag integer array
+         * @since v1.0
+         */
+        this.bagPair = bagPair;
+    }
 
     //draw pebble from a chosen bag method
     public int drawPebble() {
         /**
-         *
+         * @return int ,return the pebble that someone drawn
+         * the pebble returned is erased form the integer list array as its removed form the bag so it's somewhat an abstraction
+         * @since v1.0
          */
         if (this.getPebbles().size() == 0) {
             //return -1 if empty list
@@ -49,7 +70,8 @@ public class Bag {
     //method for discarding pebble into selected bag from a selected player
     public void discardPebble(int replacementPebble) {
         /**
-         *
+         * @param replecemenPebble, is an int which is the pebble a user has discarded and goes into a bag(this method is only used for white bag instances
+         * @since v1.0
          */
         this.getPebbles().add(replacementPebble);
     }
@@ -57,9 +79,10 @@ public class Bag {
     //method to empty a white bag into an empty black bag
     public void refillBag() {
         /**
-         *
+         *used to repopulate the bag with pebbles from the white bag once the black bag is empty
+         * @since v1.0
          */
-        this.setPebbles(this.getBagPair().getPebbles());
-        this.getBagPair().getPebbles().clear();
+        this.setPebbles(this.getBagPair().getPebbles());//checks which bag pair the pebbles have to come from
+        this.getBagPair().getPebbles().clear();//clears the corresponding bag pair as it's been refilled
     }
 }
