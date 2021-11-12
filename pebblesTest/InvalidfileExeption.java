22f187383379c2216e1a9b00ee3df0bcc72c4000
JSzafarski <92590150+JSzafarski@users.noreply.github.com>
2021-11-12 15:02:30 +0000

    finished

--- pebblesTest/InvalidfileExeption.java
+++
@@ -1,8 +0,0 @@
-public class InvalidfileExeption extends Exception {
-
-    public InvalidfileExeption(String errormsg) {
-        super(errormsg);
-        //gets a string with the error passed to it.
-    }
-
-}
